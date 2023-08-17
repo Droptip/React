71710bb477d8abc0036b21e7f323cd0b28e66f73
Droptip <dovile.kieliute@gmail.com>
2023-08-17 16:27:57 +0300

    2nd task with lizard added

---
+++ 2_paskaita/src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./components/Card"
+
+const App = () => {
+
+  return (
+  <div>
+    <ProjectCard
+        image: "./images/lizaed_1.jpg",
+        title: "Lizard",
+        subtitle: "Lizards are a widespread group of squamate reptiles, with over 7,000 species, ranging across all continents except Antarctica"
+
+    />
+   <br />
+        image: "./images/lizaed_2.jpg",
+        title: "Salamandra",
+        subtitle: "It is black with yellow spots or stripes to a varying degree; some specimens can be nearly completely black while on others the yellow is dominan"
+  </div>
+  );
+};
+
+export default App;
