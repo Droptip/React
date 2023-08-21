bc79204a23124ce4799015dcc93d86c6c61a51d4
Droptip <dovile.kieliute@gmail.com>
2023-08-21 19:09:00 +0300

    Task with title added

---
+++ 4_paskaita/src/App.jsx
@@ -0,0 +1,23 @@
+import ColorBox from "./components/ColorBox";
+import Greeting from "./components/Greeting";
+import AdultChecker from "./components/AdultChecker";
+import NotificationCount from "./components/NotificationCount";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Greeting isLoggedIn={false} />
+      <ColorBox color="red" />
+      <ColorBox color="blue" />
+      <ColorBox color="yellow" />
+      <AdultChecker age={15} />
+      <AdultChecker age={25} />
+      <NotificationCount count={50} />
+      <NotificationCount count={150} />
+      <Hero title="Info Hero" subtitle="Info subtitle" />
+    </div>
+  );
+};
+
+export default App;
